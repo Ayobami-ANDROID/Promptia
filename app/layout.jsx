33ee5ea28bfc7169c96296ce519263346a458e82
Ayobami-ANDROID <ajetunmobiayobami19@gmail.com>
2024-05-08 01:56:08 +0100

    next

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,28 +1,29 @@
-import '../styles/globals.css'
-import Nav from '../components/Nav'
-import Provider from '../components/Provider'
-
+import "../styles/globals.css";
+import Nav from "../components/Nav";
+import Provider from "../components/Provider";
 
 export const metadata = {
-    title:"Promptopia",
-    description : "Discover and Share AI Prompte"
-}
+  title: "Promptopia",
+  description: "Discover and Share AI Prompte",
+};
 
-const RootLayout = ({children}) => {
+const RootLayout = ({ children }) => {
   return (
     <html lang="en">
-        <body>
-            <div className='main'>
-                <div className='gradient'></div>
-            </div>
+      <body>
+        <Provider>
+          <div className="main">
+            <div className="gradient"></div>
+          </div>
 
-            <main className='app'>
-                <Nav/>
-                {children}
-            </main>
-        </body>
+          <main className="app">
+            <Nav />
+            {children}
+          </main>
+        </Provider>
+      </body>
     </html>
-  )
-}
+  );
+};
 
-export default RootLayout
+export default RootLayout;
--- components/Nav.js
+++ components/Nav.js
@@ -43,7 +43,7 @@ const Nav = () => {
             </button>
             <Link href="/profile">
               <Image
-                src="/assests/images/logo.svg"
+                src="/assets/images/logo.svg"
                 width={37}
                 height={37}
                 className="rounded-full"
@@ -86,18 +86,26 @@ const Nav = () => {
                   className="dropdown_link"
                   onClick={() => setToggleDropDown(false)}
                 >
-                    My profile
+                  My profile
                 </Link>
                 <Link
                   href="/create-prompt"
                   className="dropdown_link"
                   onClick={() => setToggleDropDown(false)}
                 >
-                    Create Prompt
+                  Create Prompt
                 </Link>
-                <button type="button" ></button>
+                <button
+                  type="button"
+                  onClick={() => {
+                    setToggleDropDown(false);
+                    signOut();
+                  }}
+                  className="mt-5 w-full black_btn"
+                >
+                  signOut
+                </button>
               </div>
-              
             )}
           </div>
         ) : (
